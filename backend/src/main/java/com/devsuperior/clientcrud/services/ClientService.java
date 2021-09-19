be279b0216a89d99680b8b55484c8ecac0968d4e
augusto cesar <acs-03@hotmail.com>
2021-09-19 20:26:05 -0300

    Client service

---
+++ backend/src/main/java/com/devsuperior/clientcrud/services/ClientService.java
@@ -0,0 +1,60 @@
+package com.devsuperior.clientcrud.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.clientcrud.dto.ClientDTO;
+import com.devsuperior.clientcrud.entities.Client;
+import com.devsuperior.clientcrud.repositories.ClientRepository;
+
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);		
+		return list.map(x -> new ClientDTO(x));		
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(	);
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {		
+		Client entity = repository.getOne(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setIncome(dto.getIncome());
+		entity.setChildren(dto.getChildren());
+		
+	}	
+}
