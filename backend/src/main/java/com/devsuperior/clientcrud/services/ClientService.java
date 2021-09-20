2cceb27509019dcc47d3fa86f3dfc034708b6558
augusto cesar <acs-03@hotmail.com>
2021-09-19 21:43:05 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/clientcrud/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/clientcrud/services/ClientService.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.clientcrud.dto.ClientDTO;
 import com.devsuperior.clientcrud.entities.Client;
 import com.devsuperior.clientcrud.repositories.ClientRepository;
 
+@Service
 public class ClientService {
 
 	@Autowired
